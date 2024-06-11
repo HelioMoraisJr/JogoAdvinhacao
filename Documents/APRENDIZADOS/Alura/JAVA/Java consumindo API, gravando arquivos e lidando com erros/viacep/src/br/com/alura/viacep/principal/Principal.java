dd210865ca3a9ae92adee4f603d29df98bd8b56d
HelioMoraisJr <heliomorais12@gmail.com>
2024-06-11 14:40:28 -0300

    Segundo commit. Melhorando a logica do codigo.

--- Documents/APRENDIZADOS/Alura/JAVA/Java consumindo API, gravando arquivos e lidando com erros/viacep/src/br/com/alura/viacep/principal/Principal.java
+++ Documents/APRENDIZADOS/Alura/JAVA/Java consumindo API, gravando arquivos e lidando com erros/viacep/src/br/com/alura/viacep/principal/Principal.java
@@ -1,47 +1,35 @@
 package br.com.alura.viacep.principal;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-import br.com.alura.viacep.buscacep.InformacoesCep;
+
+import br.com.alura.viacep.buscacep.BuscadorViaCep;
 import br.com.alura.viacep.buscacep.InformacoesViaCep;
+
 
 public class Principal {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		Scanner leitor = new Scanner(System.in);
+		BuscadorViaCep consultaCep = new BuscadorViaCep();
 		
 		System.out.println("Digite o CEP:");
-		var busca  = leitor.nextLine();
-		leitor.close();
-				
-		String endereco = "http://viacep.com.br/ws/" +busca+ "/json/";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        
-        String json = response.body();
-        
-        Gson gson = new GsonBuilder().create();
-        
- 		InformacoesViaCep informacoesViaCep  = gson.fromJson(json, InformacoesViaCep.class);
-
-        
-        
-        InformacoesCep meuTitulo = new InformacoesCep(informacoesViaCep);            
-        System.out.println("O Endereço é:");
-        System.out.println(meuTitulo);
+		var cep  = leitor.nextLine();
+		leitor.close();	
+		
+        try {
+        	InformacoesViaCep novoCep = consultaCep.buscaCep(cep);
+        	System.out.println(novoCep);
+        } catch (RuntimeException  e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação");
+        }
+ 		
+ 		
+           
               
      
 
